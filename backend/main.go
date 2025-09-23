ca1c4f84483abd7ea53604293636e3fb9a77d2df
aCertainProgrammer <a_certain_programmer@proton.me>
2025-09-23 12:48:57 +0200

    more cors allows

--- backend/main.go
+++ backend/main.go
@@ -28,8 +28,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	allowedOrigins := []string{
 		"https://acertainprogrammer.github.io/UnnamedDraftingTool",
+		"https://acertainprogrammer.github.io/UnnamedDraftingTool/",
 		"http://localhost:8080",
+		"http://localhost:8080/",
 		"http://127.0.0.1:8080",
+		"http://127.0.0.1:8080/",
 	}
 
 	for _, allowed := range allowedOrigins {
